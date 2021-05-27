0de87e7df43bef782e089660870654b92922b53d
masayu18 <masayushpr@gmail.com>
2021-05-27 23:12:01 +0700

    ganti config

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/lira_ponsel-main/';
+$config['base_url'] = 'http://localhost/lira_ponsel/';
 
 /*
 |--------------------------------------------------------------------------
